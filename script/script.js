a64b8600e0313f2c2bd7013006cdd6151ff5a07f
Eusébio Alves <eusebio.a95@hotmail.com>
2023-08-27 16:14:31 +0100

    Looped through repetitive code

--- script/script.js
+++ script/script.js
@@ -8,6 +8,14 @@ const myLibrary = [
         maxPage: 233,
         isRead: false,
         img: ''
+    }, {
+        id: 1,
+        title: 'Ulisses',
+        author: 'John Doe',
+        currentPage: 102,
+        maxPage: 233,
+        isRead: false,
+        img: ''
     }
 ];
 
@@ -53,9 +61,11 @@ myLibrary.forEach(function(book){
     let title = document.createElement('h3');
     title.classList.add('title');
     title.innerText = book.title;
+
     let author = document.createElement('p');
     author.classList.add('author');
     author.innerText = `By ${book.author}`;
+
     let pages = document.createElement('p');
     pages.classList.add('pages');
     pages.innerText = `Currently on page ${book.currentPage} of ${book.maxPage}.`;
@@ -67,51 +77,53 @@ myLibrary.forEach(function(book){
 
     //optns Children START
     let hr = document.createElement('hr');
-    let optnsRow1 = document.createElement('div');
-    optnsRow1.classList.add('optns-row');
-    let optnsRow2 = document.createElement('div');
-    optnsRow2.classList.add('optns-row');
-    let optnsRow3 = document.createElement('div');
-    optnsRow3.classList.add('optns-row');
-
     optns.appendChild(hr);
-    optns.appendChild(optnsRow1);
-    optns.appendChild(optnsRow2);
-    optns.appendChild(optnsRow3);
+
+    let optnRows = [];
+    for (let i = 1; i < 4; i++) {
+        optnRows[i] = document.createElement('div');
+        optnRows[i].classList.add('optns-row');
+
+        optns.appendChild(optnRows[i]);
+    }
     //optns Children END
 
     //optnsRows Children START
-        let btn1 = document.createElement('button');
-        let btn2 = document.createElement('button');
-        let btn3 = document.createElement('button');
-        let btn4 = document.createElement('button');
-        let btn5 = document.createElement('button');
-        btn1.setAttribute('type', 'button');
-        btn2.setAttribute('type', 'button');
-        btn3.setAttribute('type', 'button');
-        btn4.setAttribute('type', 'button');
-        btn5.setAttribute('type', 'button');
-        btn1.classList.add('add-page');
-        btn2.classList.add('remove-page');
-        btn3.classList.add('read');
-        btn4.classList.add('del');
-        btn5.classList.add('edit');
-        btn1.innerText = 'Current page +';
-        btn2.innerText = 'Current page -';
-        btn3.innerText = 'Toggle read';
-        btn4.innerText = 'Delete book';
-        btn5.innerText = 'Edit';
+    let btns = [];
 
-        optnsRow1.appendChild(btn1);
-        optnsRow1.appendChild(btn2);
+    for (let i = 1; i < 6; i++) {
+        btns[i] = document.createElement('button');
+        btns[i].setAttribute('type', 'button');
 
-        optnsRow2.appendChild(btn3);
-        optnsRow2.appendChild(btn4);
-
-        optnsRow3.appendChild(btn5);
+        switch(i) {
+            case 1:
+                btns[i].classList.add('add-page');
+                btns[i].innerText = 'Current page +';
+                optnRows[1].appendChild(btns[i]);
+                break;
+            case 2:
+                btns[i].classList.add('remove-page');
+                btns[i].innerText = 'Current page -';
+                optnRows[1].appendChild(btns[i]);
+                break;
+            case 3:
+                btns[i].classList.add('read');
+                btns[i].innerText = 'Toggle read';
+                optnRows[2].appendChild(btns[i]);
+                break;
+            case 4:
+                btns[i].classList.add('del');
+                btns[i].innerText = 'Delete book';
+                optnRows[2].appendChild(btns[i]);
+                break;
+            case 5: 
+                btns[i].classList.add('edit');
+                btns[i].innerText = 'Edit';
+                optnRows[3].appendChild(btns[i]);
+                break;
+        }
+    }
     //optnsRows Children END
 
     //Creating elements, setting classes/ID/content & appending to father elements END
-
-    console.log(book);
 });
