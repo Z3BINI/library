c47d705ceee744ad3c769c20d4e6266ad382c382
Eusébio Alves <eusebio.a95@hotmail.com>
2023-08-27 14:41:10 +0100

    Added library array, book contructor & started add book function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-console.log('yea buddy');
+const myLibrary = [];
+
+function Book(title, author, currentPage, maxPage, isRead, img) {
+    this.id = myLibrary.length;
+    this.title = title;
+    this.author = author;
+    this.currentPage = currentPage;
+    this.maxPage = maxPage;
+    this.isRead = isRead;
+    this.img = img ?? '';
+}
+
+function addBookToLibrary() {
+    
+}
